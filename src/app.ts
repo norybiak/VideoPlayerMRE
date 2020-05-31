f7479a89cdc27305a459e0bf56664e2dcb9cbdc8
NorybiaK <norybiak@gmail.com>
2020-05-31 05:38:28 -0700

    remove ms copyright

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,3 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 import Server from './server';
 
