5ed246f7c762472278941276ccca4ae63b999ae7
hreid3 <horace.reid@bluenetreflections.com>
2021-12-29 13:08:20 -0500

    Trying to single mode working.

--- src/app2.ts
+++ src/app2.ts
@@ -20,6 +20,7 @@ export default class LiveStreamVideoPlayer {
 	private modeNoNewJoins = false;
 	private attach = false;
 	private appStarted = false;
+	private videoActor: MRE.Actor;
 
 	constructor(private context: MRE.Context, private params: MRE.ParameterSet) {
 		this.assets = new MRE.AssetContainer(context);
@@ -30,13 +31,13 @@ export default class LiveStreamVideoPlayer {
 		if (!this.isClientValid()) { return; }
 		this.context.onStarted(async () => {
 			if (!this.isClientValid()) { return; }
-			console.log(new Date(), "App started:", context.sessionId);
+			console.log(new Date(), "App starting:", context.sessionId);
 			this.userMediaState = null;
 			const videoStream1 = this.assets.createVideoStream(
 				'stream1',
 				{
 					// uri: 'http://108.72.45.167:8080/tmp_dash/stream/index.mpd',
-					uri: 'http://108.72.45.167:8080/tmp_hls/stream/index.m3u8',
+					uri: 'http://108.72.45.167:8080/hls/stream/index.m3u8',
 					// uri: 'http://service-stitcher.clusters.pluto.tv/stitch/hls/channel/569546031a619b8f07ce6e25/master.m3u8?advertisingId=&appName=web&appVersion=unknown&appStoreUrl=&architecture=&buildVersion=&clientTime=0&deviceDNT=0&deviceId=2aaaf380-c2a0-11eb-b95a-9564040e8ac6&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&sid=2a276526-cf0b-433e-b553-654032d0c7a8&userId=&serverSideAds=true&yyy=index.m3u8',
 					duration: 0,
 				}
@@ -45,6 +46,8 @@ export default class LiveStreamVideoPlayer {
 			this.videoStreams = [videoStream1];
 			this.root = MRE.Actor.Create(this.context, {actor: {name: 'bigscreen-Root'}});
 			await this.root.created;
+			await this.init();
+			console.log(new Date(), "App started:", context.sessionId);
 		});
 
 		this.context.onUserJoined((user) => this.handleUserJoined(user));
@@ -72,8 +75,6 @@ export default class LiveStreamVideoPlayer {
 	}
 
 	private async handleUserJoined(user: MRE.User) {
-		await this.init();
-
 		await block(() => this.appStarted, 30000);
 		if (!this.isClientValid()) { return; }
 		console.log(
@@ -91,6 +92,8 @@ export default class LiveStreamVideoPlayer {
 			console.log(new Date(), `User ${user.name} blocked`);
 			return;
 		}
+		this.CreateStreamInstance(this.videoActor);
+
 	}
 
 	private handleUserLeft(user: MRE.User) {
@@ -106,43 +109,48 @@ export default class LiveStreamVideoPlayer {
 		// }
 	}
 
+	private getVideoActor = () => {
+		const transform = {
+			local: {
+				position: { x: 0, y: 0, z: 0 },
+				scale: { x: 2, y: 2, z: 2 },
+			}
+		}
+		// if (this.attach) {
+		// 	const scaleFactor = 0.85;
+		// 	transform.local.position.z = 1;
+		// 	transform.local.position.y = 0.25;
+		// 	transform.local.scale = { x: scaleFactor, y: scaleFactor, z: scaleFactor};
+		// }
+		return MRE.Actor.Create(this.context, {
+			actor: {
+				// exclusiveToUser: user.id,
+				// appearance: { enabled: groupMask },
+				parentId: this.root.id,
+				name: `big-screen-video-0`,
+				// light: { type: 'point', intensity: 2.5, range: 50, enabled: true, spotAngle: 180, color: MRE.Color3.White() }, // Add a light component.
+				transform,
+				// rigidBody: this.attach ? { isKinematic: true } : undefined,
+				collider: this.attach ? { bounciness: 10, geometry: { shape: ColliderType.Auto } } : undefined,
+			}
+		});
+
+	}
+
 	private async init() {
 		if (!this.appStarted) {
-			const transform = {
-				local: {
-					position: { x: 0, y: 0, z: 0 },
-					scale: { x: 2, y: 2, z: 2 },
-				}
-			}
-			// if (this.attach) {
-			// 	const scaleFactor = 0.85;
-			// 	transform.local.position.z = 1;
-			// 	transform.local.position.y = 0.25;
-			// 	transform.local.scale = { x: scaleFactor, y: scaleFactor, z: scaleFactor};
-			// }
-			const videoActor = MRE.Actor.Create(this.context, {
-				actor: {
-					// exclusiveToUser: user.id,
-					// appearance: { enabled: groupMask },
-					parentId: this.root.id,
-					name: `big-screen-video-0`,
-					// light: { type: 'point', intensity: 2.5, range: 50, enabled: true, spotAngle: 180, color: MRE.Color3.White() }, // Add a light component.
-					transform,
-					// rigidBody: this.attach ? { isKinematic: true } : undefined,
-					collider: this.attach ? { bounciness: 10, geometry: { shape: ColliderType.Auto } } : undefined,
-				}
-			});
+			const videoActor = this.getVideoActor();
 			await Promise.all([videoActor.created()]);
+			this.videoActor = videoActor;
 			// if (this.attach) {
 			// 	videoActor.attach(user.id, 'center-eye');
 			// }
-			this.CreateStreamInstance(videoActor);
 			this.appStarted = true;
 		}
 	}
 
 	private CreateStreamInstance(parentActor: MRE.Actor) {
-		this.userMediaState?.mediaInstance.stop();
+		// this.userMediaState?.mediaInstance?.stop();
 		const soundOptions: CustomSetVideoStateOptions = {
 			volume: 0.7,
 			looping: false,
